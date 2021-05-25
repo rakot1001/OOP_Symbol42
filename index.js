dbe37f3b9c50d4015a128b743d80a084699b7008
Vlad <vladdp70@gmail.com>
2021-05-25 07:11:32 +0300

    Список(капец)

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const mySymbol = Symbol();
+class ListItem {
+  constructor(v) {
+    this._value = v;
+    this.next = null;
+    this.prev = null;
+  }
 
-const obj ={
-  login: 'Vas12',
-  mySymbol: 'value',
-  [mySymbol]: 100
-};
+  get value() {
+    return this._value;
+  }
+
+  set value(v) {
+    this._value = v;
+  }
+}
+
+class LinkedList {
+  constructor(...args) {
+    this.head = null;
+    this.tale = null;
+    for (const item of args) {
+      this.push(item);
+    }
+  }
+  push(v) {
+    const newItem = new ListItem(v);
+
+    if (this.length === 0) {
+      this.head = newItem;
+      this.tale = newItem;
+    } else{
+      this.tale.next = newItem;
+      newItem.prev = this.tale;
+      this.tale = newItem;
+    }
+
+    return ++this.length;
+  }
+}
